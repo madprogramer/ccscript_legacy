07797075e9688c502fa3baed125dcb51922383c0
madprogramer <themadprogramer@gmail.com>
2017-10-01 12:26:42 +0300

    This will do it
    hopefully

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(name="ccscript",
       version="1.338",
       description="ccscript",
       url="http://starmen.net/pkhack/ccscript",
-      
       ext_modules=[
           Extension("ccscript",
                     source_files,
